acf745b8f2e0ebffdfdc090f3d5e4852289534bc
daggzu <diegog8603@gmail.com>
2023-12-15 12:20:06 -0400

    changes to 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
+"""Module starts a Flask web application"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
-app = Flask(__name__)
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+app = Flask(__name__)  # Holds the name of the module.
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(self):
+    """Method for closing Flask connection"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Function returns a HTML page with a list
+    of states when routed to
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Runs the application.
