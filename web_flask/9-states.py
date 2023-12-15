8cd4a2ca2bcc975e8151148512d77d8c774ad44f
daggzu <diegog8603@gmail.com>
2023-12-15 18:52:39 -0400

    checking

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
-""" This script that starts a Flask web application """
-
+"""
+Starts a Flask web application.
+"""
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states')
-def states():
-    states = storage.all(State)
-    return render_template('9-states.html', states=states)
-
-
-@app.route('/states/<id>')
-def states_id(id):
-    states = storage.all(State).values()
-    state = next((state for state in states if state.id == id), None)
-    if state is not None:
-        cities = sorted(state.cities, key=lambda city: city.name)
-    else:
-        cities = []
-    return render_template('9-states.html', state=state, cities=cities)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
