856a0cace8324d881434e433ee1140024d27df04
daggzu <diegog8603@gmail.com>
2023-12-15 19:16:25 -0400

    fixing 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-"""
+""""Module set to load cities of states"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
-app = Flask(__name__)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+app = Flask(__name__)  # Holds the name of the module.
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(self):
+    """Method for closing Flask connection"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    Function returns a HTML page with a list
+    of states when routed to
+    """
+    states = storage.all(State)
+    states_cities = list(states.values())
+    return render_template('8-cities_by_states.html', states=states_cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Runs the application.
