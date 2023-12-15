a40064c56ea3116636bf51d1316d022bf739c989
daggzu <diegog8603@gmail.com>
2023-12-15 12:45:05 -0400

    testing something

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,88 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Module starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
-    /number/<n>: Displays 'n is a number' only if <n> is an integer.
-    /number_template/<n>: Displays an HTML page only if <n> is an integer.
-    /number_odd_or_even/<n>: Displays an HTML page only if <n> is an integer.
-        - States whether <n> is even or odd in the body.
-"""
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
+app = Flask(__name__)  # Holds the name of the module.
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>
-
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>
-
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """Displays 'n is a number' only if <n> is an integer."""
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    """Displays an HTML page only if <n> is an integer."""
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even(n):
-    """Displays States whether <n> is odd or even in the body"""
-    odd_or_even = "odd" if n % 2 else "even"
-    return render_template("6-number_odd_or_even.html",
-                           n=n, odd_or_even=odd_or_even)
+@app.teardown_appcontext
+def teardown_db(self):
+    """Method for closing Flask connection"""
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page"""
-    states_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=states_li)
-
-
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """After each request, remove the SQLAlchemy Session"""
-    storage.close()
+    """
+    Function returns a HTML page with a list
+    of states when routed to
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # Runs the application.
