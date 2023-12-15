1dcbcdec5e33372c94dd920a60027443f5ca6353
daggzu <diegog8603@gmail.com>
2023-12-15 18:09:21 -0400

    html

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
-""" This script that starts a Flask web application """
-
+"""Module starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
-from models.state import State
-
+from models import storage, State
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ Route that display a HTML page with a list of states
-    objects sorted by name """
-    state_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=state_li)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Route that display a HTML page with a list of cities
-    objects sorted by name """
-    city_li = storage.all(State).values()
-    return render_template('8-cities_by_states.html', cities=city_li)
-
-
 @app.route('/states', strict_slashes=False)
+def states():
+    """
+    Function returns a HTML page with a
+    list of states when routed to.
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('9-states.html', states=states)
+
+
 @app.route('/states/<id>', strict_slashes=False)
-def states(id=None):
-    state_dic = storage.all(State)
-    state = None
-    for obj in state_dic.values():
-        if obj.id == id:
-            state = obj
-    return render_template('9-states.html', states=state_dic, id=id,
-                           state=state)
+def state(id):
+    """
+    Function returns a HTML page with a
+    list of states when routed to.
+    """
+    state = storage.get(State, id)
+    if state is not None:
+        cities = sorted(state.cities, key=lambda city: city.name)
+        return render_template('9-state.html',
+                               state=state, cities=cities)
+    else:
+        return render_template('404.html'), 404
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ Function that removes the current SQL Alchemy Session after each
-    request. """
+def teardown_db(self):
+    """Method for closing Flask connection"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
